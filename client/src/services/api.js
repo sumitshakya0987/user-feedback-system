672f7c35b0c2720448e68bffc501fc27a948260e
sumitshakya0987 <shakyasumit@gmail.com>
2025-04-27 12:36:48 +0530

    Your message about what you changed

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000', // Backend runs on 5000
+  baseURL: 'https://user-feedback-system-zeta.vercel.app/', // Backend runs on 5000
 });
 
 // Submit feedback
